2130ec34091efe6dffc28ca51f7383d4a62bd183
eric ogbe <tradelearn13@gmail.com>
2021-07-01 10:04:29 +0100

    Create Motor.java

---
+++ Motor.java
@@ -0,0 +1,21 @@
+package com.qa.garage;
+
+public class Motor extends Car {
+     private String brand;
+     private boolean helmet;
+     
+public String getBrand() {
+return brand;
+}
+public void setBrand(String brand) {
+this.brand = brand;
+}
+public boolean isHelmet() {
+return helmet;
+}
+public void setHelmet(boolean helmet) {
+this.helmet = helmet;
+}
+     
+     
+}
