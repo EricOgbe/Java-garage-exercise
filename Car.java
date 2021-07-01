a889bdc00a7601be79ef34d09875a51174e91d03
eric ogbe <tradelearn13@gmail.com>
2021-07-01 10:05:10 +0100

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,21 @@
+package com.qa.garage;
+
+public class Car extends Vehicle{
+ 
+ 
+   private int speed;
+   private int numDoors;
+public int getSpeed() {
+return speed;
+}
+public void setSpeed(int speed) {
+this.speed = speed;
+}
+public int getNumDoors() {
+return numDoors;
+}
+public void setNumDoors(int numDoors) {
+this.numDoors = numDoors;
+}
+       
+}
