995e82c156ca07ad2b7431d269937a1e96aa6fea
eric ogbe <tradelearn13@gmail.com>
2021-07-01 10:03:52 +0100

    Create Scooter.java

---
+++ Scooter.java
@@ -0,0 +1,19 @@
+package com.qa.garage;
+
+public class Scooter extends Motor {
+    private int brakes;
+    private double weight;
+public int getBrakes() {
+return brakes;
+}
+public void setBrakes(int brakes) {
+this.brakes = brakes;
+}
+public double getWeight() {
+return weight;
+}
+public void setWeight(double weight) {
+this.weight = weight;
+}
+   
+}
