c84cf38081737a366380955fb9409e625b8a7456
eric ogbe <tradelearn13@gmail.com>
2021-07-01 10:03:08 +0100

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,25 @@
+package com.qa.garage;
+
+public class Vehicle {
+private String name;
+     private int wheels;
+     private String color;
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public int getWheels() {
+return wheels;
+}
+public void setWheels(int wheels) {
+this.wheels = wheels;
+}
+public String getColor() {
+return color;
+}
+public void setColor(String color) {
+this.color = color;
+}
+}
